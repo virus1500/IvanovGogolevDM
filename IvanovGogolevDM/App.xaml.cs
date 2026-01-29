457b21cdae48bc1ed2499773f0e67b1146f6f003
vboxuser <vboxuser@WIN10>
2026-01-29 12:14:44 +0300

    Добавьте файлы проекта.

---
+++ IvanovGogolevDM/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using IvanovGogolevDM.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace IvanovGogolevDM
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {  
+            public static IvanovGogolevBDEntities context = new IvanovGogolevBDEntities();
+    }
+}
+
